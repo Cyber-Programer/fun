b8e633b4ada0adcf6dbeb1400f14a5df301f436a
MD siFaT isLaM <125746506+Cyber-Programer@users.noreply.github.com>
2023-09-17 12:34:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
 function go(){
-    window.location.href = 'https://cyber-programer.github.io/first_web'
+    window.location.href = 'https://cyber-programer.github.io/first_web';
 }
 
 function main(){
-    const repot = document.getElementById('comment')
+    const repot = document.getElementById('comment');
     const user = document.getElementById('enter').value.toLowerCase();
     if (!user){
-        alert('Enter you name')
+        alert('Enter your name');
         return;
-    }else{
+    } else {
         if (user === 'sifat'){
-            repot.innerHTML = 'Admin'
-        } else{
+            repot.innerHTML = 'Admin';
+        } else {
             if(user === 'shohan'){
-                repot.innerHTML = 'Pure single boy'
+                repot.innerHTML = 'Pure single boy';
             } else if(user === 'apon'){
-                repot.innerHTML = 'All unmarried girls of crush 🤭'
+                repot.innerHTML = 'All unmarried girls of crush 🤭';
             } else if(user === 'hridoy'){
-                repot.innerHTML = 'Mey potanor try kari 🤐'
+                repot.innerHTML = 'Mey potanor try kari 🤐';
             } else if(user === 'akiba'){
-                repot.innerHTML = 'sifat er bestu 😍'
+                repot.innerHTML = 'sifat er bestu 😍';
             } else if(user === 'mariya'){
-                repot.innerHTML  = 'Jony kakku er fan 🤐'
-            } else if(user== 'sharmin'){
-                repot.innerHTML = 'Half married 😐'
+                repot.innerHTML  = 'Jony kakku er fan 🤐';
+            } else if(user === 'sharmin'){
+                repot.innerHTML = 'Half married 😐';
             } else if(user == 'sumiya'|| user =='sumiya'){
-                repot.innerHTML = 'Collage pass 🤨'
+                repot.innerHTML = 'Collage pass 🤨';
             } else if(user == 'naima' || user ==  'nayma'){
-                repot.innerHTML = 'Cheka khor 😶'
+                repot.innerHTML = 'Cheka khor 😶';
             } else if(user == 'rony'|| user == 'roni'){
-                repot.innerHTML = 'Kuttar haddi 😏 sorry faziler*'
-            }else if(user == 'habiba'){
-                repot.innerHTML = 'Choto morich e jhal besi 😌'
-            }else if(user =='khusbu'){
-                repot.innerHTML ='sir re bichar dimu 😎'
-            }else if(user == 'khadiza'|| user == 'khadija'){
-                repot.innerHTML = 'Apon er bestie 🤭'
-            }else if(user == 'rimon'){
-                repot.innerHTML = 'Ekta na hoile arekta 🤓'
-            }else if(user == 'ahosan'){
-                repot.innerHTML = 'Sobar pola 😟'
-            }else if(user =='niloy'){
-                repot.innerHTML = 'Gorila 🤭'
-            }else if(user == 'shorna'){
-                repot.innerHTML = 'Lolita 😌'
-            }else if(user == 'rafa'){
-                repot.innerHTML = 'Unknown persone 😏'
-            }else if(user == 'tasrun'){
-                repot.innerHTML = 'Choto vai amr'
-            }else if(user == 'hasan'||user == 'hasan sir'){
-                repot.innerHTML = 'Fevourit sir 😍'
-            }else if(user == 'ruhulamin'||user == 'ruhul amin'{
-                repot.innerHTML = 'Big boss'
+                repot.innerHTML = 'Kuttar haddi 😏 sorry faziler*';
+            } else if(user == 'habiba'){
+                repot.innerHTML = 'Choto morich e jhal besi 😌';
+            } else if(user =='khusbu'){
+                repot.innerHTML ='sir re bichar dimu 😎';
+            } else if(user == 'khadiza'|| user == 'khadija'){
+                repot.innerHTML = 'Apon er bestie 🤭';
+            } else if(user == 'rimon'){
+                repot.innerHTML = 'Ekta na hoile arekta 🤓';
+            } else if(user == 'ahosan'){
+                repot.innerHTML = 'Sobar pola 😟';
+            } else if(user =='niloy'){
+                repot.innerHTML = 'Gorila 🤭';
+            } else if(user == 'shorna'){
+                repot.innerHTML = 'Lolita 😌';
+            } else if(user == 'rafa'){
+                repot.innerHTML = 'Unknown person 😏';
+            } else if(user == 'tasrun'){
+                repot.innerHTML = 'Choto vai amr';
+            } else if(user == 'hasan'||user == 'hasan sir'){
+                repot.innerHTML = 'Favorite sir 😍';
+            } else if(user == 'ruhulamin'||user == 'ruhul amin'){
+                repot.innerHTML = 'Big boss';
             }
         }
     }
