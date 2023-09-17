c60fbf7ac33c9a0733068dc00eb990fa3581c0c2
MD siFaT isLaM <125746506+Cyber-Programer@users.noreply.github.com>
2023-09-17 10:32:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -15,17 +15,33 @@ function main(){
             if(user === 'shohan'){
                 repot.innerHTML = 'Pure single boy'
             } else if(user === 'apon'){
-                repot.innerHTML = 'Khadur friend'
+                repot.innerHTML = 'Khadur jamai 🤭'
             } else if(user === 'hridoy'){
-                repot.innerHTML = 'Mey potanor try kari'
+                repot.innerHTML = 'Mey potanor try kari 🤐'
             } else if(user === 'akiba'){
-                repot.innerHTML = 'sifat er bestu'
+                repot.innerHTML = 'sifat er bestu 😍'
             } else if(user === 'mariya'){
-                repot.innerHTML  = 'Jony kakku er fan'
+                repot.innerHTML  = 'Jony kakku er fan 🤐'
             } else if(user== 'sharmin'){
-                repot.innerHTML = 'Half married'
+                repot.innerHTML = 'Half married 😐'
             } else if(user == 'sumiya'){
-                repot.innerHTML = 'Collage pass'
+                repot.innerHTML = 'Collage pass 🤨'
+            } else if(user == 'naima' || user ==  'nayma'){
+                repot.innerHTML = 'Cheka khor 😶'
+            } else if(user == 'rony'|| user == 'roni'){
+                repot.innerHTML = 'Biri khor 😏'
+            }else if(user == 'habiba'){
+                repot.innerHTML = 'Choto morich e jhal besi 😌'
+            }else if(user =='khusbu'){
+                repot.innerHTML ='sir re bichar dimu 😎'
+            }else if(user == 'khadiza'){
+                repot.innerHTML = 'Jisan er bou 🤭'
+            }else if(user == 'rimon'){
+                repot.innerHTML = 'Ekta na hoile arekta 🤓'
+            }else if(user == 'ahosan'){
+                repot.innerHTML = 'Sobar pola 😟'
+            }else if(user =='niloy'){
+                repot.innerHTML = 'Gorila 🤭'
             }
         }
     }
