5d6aaef0f6099fe487af000b2b25658edeed06d6
MD siFaT isLaM <125746506+Cyber-Programer@users.noreply.github.com>
2023-12-20 09:29:18 -0500

    Update s.js

--- pass/s.js
+++ pass/s.js
@@ -5,9 +5,9 @@ var pass = document.querySelector('#pass')
 eay.addEventListener('click',function(){
     if(pass.type == 'password'){
         pass.type = 'text'
-        eay.src = '/img/eye-open.png'
+        eay.src = './img/eye-open.png'
     }else{
         pass.type = 'password'
-        eay.src = '/img/eye-close.png'
+        eay.src = './img/eye-close.png'
     }
 })
