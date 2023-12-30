5cd22fea43158e593270b3f77241d2d9eb15ebf3
MD siFaT isLaM <125746506+Cyber-Programer@users.noreply.github.com>
2023-12-30 04:02:03 -0500

    Add files via upload

---
+++ clone-site/s.js
@@ -0,0 +1,11 @@
+const bars = document.querySelector('.nav-links')
+
+function show(){
+    bars.style.display = 'inline-block';
+    // bars.style.right = 0
+}
+
+function hide(){
+    bars.style.display = 'none';
+    // bars.style.right = '-200px'
+}
